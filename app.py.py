2039c4e6526a6242cd883b2810c1a5fe896154db
Dorine Achieng <dorinelilian8@gmail.com>
2025-07-14 16:48:55 +0300

    Fix: Update Flask host and port for Render

--- app.py.py
+++ app.py.py
@@ -31,5 +31,10 @@ def predict():
         return jsonify({'error': str(e)}), 400
 
 # Running this took forever the first time, so I commented it and ran in cmd
+import os
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
+
+
